1fa42883dd7765bc083a5c023758576dd2790a95
Sameer <72811430+sameer-h@users.noreply.github.com>
2022-10-13 10:43:23 -0500

    Updated main.cpp for servers and runtime

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 
 using namespace std;
 
+
+Request makeReq() {
+    Request newReq;
+    newReq.randTime++; // Time will always be above 0
+    return newReq;
+}
+
 int main () {
 
 
@@ -23,24 +30,53 @@ int main () {
     cout << "Enter the amount of time the load balancer should run:" << endl;
     cin >> runtime;
 
-    Request req;
-    cout << "IP IN: " << req.ipI << endl;
-    cout << "IP OUT: " << req.ipO << endl;
-    cout << "IP TIME: " << req.randTime << endl;
+    // Request req;
+    // cout << "IP IN: " << req.ipI << endl;
+    // cout << "IP OUT: " << req.ipO << endl;
+    // cout << "IP TIME: " << req.randTime << endl;
+
+
 
     LoadBal loadBal = LoadBal();
 
     for (int i = 0; i < numReqs; i++) {
 
-        Request randReq;
+        Request randReq = makeReq();
         loadBal.pushReq(randReq);
   
     }
 
     vector<Webserver> serverVect(numServers, NULL);
 
-  
- 
+    for (int i = 0; i < numServers; i++) {
+        serverVect[i] = Webserver((char)(65 + i));
+        serverVect[i].processReq(loadBal.getReq(), loadBal.getTime());
+
+    }
+
+    while (loadBal.getTime() < runtime) {
+
+
+        if (loadBal.isQueueClear()) {
+            cout << "QUEUE IS CLEAR. EXITING..." << endl;
+            break;
+        }
+
+        int curTime = loadBal.getTime();
+
+
+        if (serverVect[curTime % numServers].reqStatus(curTime)) {
+
+            Request r = serverVect[curTime % numServers].getReq();
+            cout << serverVect[curTime % numServers].getWebserverName() << " has processed from " << r.ipI << " to " << r.ipO << " at T = " << curTime << endl;
+            serverVect[curTime % numServers].processReq(loadBal.getReq(), curTime);
+        }
+
+        loadBal.incTime();
+    }
+
+
+
     return 0;
 
 }
